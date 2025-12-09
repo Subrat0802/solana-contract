b7be298dccc2d125c88177e17714ca113e0727f7
Subrat Mishra <piyush.subrat8@gmail.com>
2025-12-09 12:35:49 +0530

    counter

--- prctice/client/instructions.ts
+++ prctice/client/instructions.ts
@@ -1,18 +1,39 @@
 import * as borsh from "borsh";
 
-
 export class CounterAccount {
     count = 0;
-
     constructor({count}: {count: number}){
-        this.count = count;
+        this.count = count
     }
 }
 
-
-export const schema = {struct: {count: 'u32'}};
+export const schema = {
+    struct: {
+        count: 'u32'
+    }
+}
 
 export const GREETING_SIZE = borsh.serialize(
     schema,
     new CounterAccount({count: 0})
 ).length
+
+
+// import * as borsh from "borsh";
+
+
+// export class CounterAccount {
+//     count = 0;
+
+//     constructor({count}: {count: number}){
+//         this.count = count;
+//     }
+// }
+
+
+// export const schema = {struct: {count: 'u32'}};
+
+// export const GREETING_SIZE = borsh.serialize(
+//     schema,
+//     new CounterAccount({count: 0})
+// ).length
