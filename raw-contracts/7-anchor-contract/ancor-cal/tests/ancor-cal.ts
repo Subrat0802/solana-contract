7bf2060799dde9676f7bffc2905319f2df338620
Subrat Mishra <piyush.subrat8@gmail.com>
2025-12-15 19:06:48 +0530

    cal contrcat

--- raw-contracts/7-anchor-contract/ancor-cal/tests/ancor-cal.ts
+++ raw-contracts/7-anchor-contract/ancor-cal/tests/ancor-cal.ts
@@ -18,4 +18,41 @@ describe("ancor-cal", () => {
     .rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("add", async () => {
+    const tx = await program.methods.add(10)
+    .accounts({
+      newAccount: newAccount.publicKey,
+      signer: anchor.getProvider().wallet.publicKey,
+    })
+    .rpc();
+    console.log("add transaction", tx);
+  })
+  it("sub", async () => {
+    const tx = await program.methods.sub(10)
+    .accounts({
+      newAccount: newAccount.publicKey,
+      signer: anchor.getProvider().wallet.publicKey,
+    })
+    .rpc();
+    console.log("sub transaction", tx);
+  })
+  it("mul", async () => {
+    const tx = await program.methods.mul(10)
+    .accounts({
+      newAccount: newAccount.publicKey,
+      signer: anchor.getProvider().wallet.publicKey,
+    })
+    .rpc();
+    console.log("mul transaction", tx);
+  })
+  it("div", async () => {
+    const tx = await program.methods.div(2)
+    .accounts({
+      newAccount: newAccount.publicKey,
+      signer: anchor.getProvider().wallet.publicKey,
+    })
+    .rpc();
+    console.log("div transaction", tx);
+  })
 });
